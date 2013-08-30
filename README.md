1e9c383231fdc9122d7d077993b805744ff7d24c
Andy Green <hello@andygrn.co.uk>
2013-08-30 17:17:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,87 @@
+# Time Machine
+
+Pushstate history and ajax helper for fancy websites. It handles:
+
+- State change detection
+- Replacing the URL
+- Loading the new page via ajax
+- Binding <a> tags to push state rather than navigate
+- Highlighting the correct new navigation item
+- Replacing the page title
+
+Navigation highlighting and page titles are done using html `data-` attributes on the ajaxed content. See below for an example.
+
+If a browser doesn't support pushState, Time Machine won't start up and the site will work normally.
+
+## Current dependencies
+
+- [History.js Native](https://github.com/browserstate/history.js)
+- [Eventie](https://github.com/desandro/eventie)
+- [Ajax.js](https://github.com/honza/ajax.js)
+
+## Example usage
+
+Site frame:
+
+```html
+<ul id="nav-primary">
+	<li data-tm-match="rad-page"><a href="rad-page">Rad Page</a></li>
+	<li data-tm-match="cool-page"><a href="cool-page">Cool Page</a></li>
+	<li data-tm-match="lame-page"><a href="lame-page">Lame Page</a></li>
+</ul>
+<div id="ajax-receptacle"></div>
+```
+
+Ajax page template:
+
+```html
+<div id="main" data-tm-title="Cool Page - Website Name" data-tm-id="cool-page">
+	<h1>Cool Page</h1>
+	<p>Nulla debitis earum impedit laboriosam minus? Officiis, maiores atque ea velit minima ex numquam quaerat quisquam? Delectus, hic porro voluptatem quod rem!</p>
+</div>
+```
+
+```javascript
+var time_machine = TimeMachine( {
+	site_root: 'http://www.website.com',
+	frameless_root: '/ajax',
+	ajax_receptacle: document.querySelector( '#ajax-receptacle' ),
+	state_change_selector: 'a',
+	metadata_element_selector: '#main',
+	nav_items: document.querySelectorAll( '#nav-primary li' ),
+	nav_selected_class: 'active',
+	debug: true,
+	beforeNewPageLoad: function(){
+		document.body.className = 'loading';
+	},
+	afterNewPageLoad: function(){
+		document.body.className = '';
+	}
+} );
+
+// Example of manual load
+time_machine.pushStateChange( 'http://www.website.com/rad-page' );
+```
+
+## Inputs
+
+- `site_root` - The site root URL (no trailing slash)
+- `ajax_receptacle` - The element the ajax template will be inserted into
+- `metadata_element_selector` - The selector of the element with page metadata attributes (`data-tm-title` and `data-tm-id`)
+- `nav_items` - A NodeList of navigation elements with page id attribute (`data-tm-match`)
+- `nav_selected_class` - The class to give navigation elements when they are active
+- `frameless_root` - (Optional, Default `''`) The subdirectory where your ajax templates are stored (opening slash, no trailing slash)
+- `state_change_selector` - (Optional, Default `'a'`) The selector(s) of `<a>`s you want to trigger state changes
+- `title_while_loading` - (Optional, Default `'Loading...'`) The page title whilst a page load is in progress
+- `beforeNewPageLoad` - (Optional) Function to run before a new page is loaded
+- `afterNewPageLoad` - (Optional) Function to run after a new page is loaded
+- `debug` - (Optional, Default `false`) Generate console messages
+
+## Methods
+
+- `pushStateChange( url )` - Manually load a new page
+
+## TODO
+
+- Cancel the current page transition if the state changes again during loading OR ignore state changes while loading is in progress
+- Test this in multiple browsers
---
+++ javascript/TimeMachine.js
@@ -0,0 +1,155 @@
+/*
+	Time Machine - Pushstate history and ajax helper
+	@andygrn 2013
+	Requires:
+		- History.js Native (https://github.com/browserstate/history.js)
+		- Eventie (https://github.com/desandro/eventie)
+		- Ajax.js (https://github.com/honza/ajax.js)
+*/
+
+( function(){
+
+	'use strict';
+
+	window.TimeMachine = function( inputs ){
+
+		var debug = inputs.debug ? true : false;
+
+		if( typeof window.History === 'undefined' || typeof window.eventie === 'undefined' || typeof window.ajax === 'undefined' ){
+			debugLog( 'One or more dependencies are missing', 'warn' );
+			return;
+		}
+
+		if( !window.History.enabled ){
+			debugLog( 'History.js is disabled', 'warn' );
+			return;
+		}
+
+		var site_root = stripTrailingSlash( inputs.site_root );
+		var frameless_root = inputs.frameless_root ? stripTrailingSlash( inputs.frameless_root ) : '';
+		var state_change_selector = inputs.state_change_selector || 'a';
+		var title_element = document.querySelector( 'title' );
+		var title_while_loading = inputs.title_while_loading || 'Loading...';
+		var regex_toggle_class = new RegExp( '(?:^|\\s)' + inputs.nav_selected_class.toString() + '(?!\\S)', 'gi' );
+
+		window.History.Adapter.bind( window, 'statechange', handleStateChange );
+		bindTriggers( document.body );
+		debugLog( 'Time Machine ready on "' + site_root + '"' );
+		debugLog( '------' );
+
+		function debugLog( message, type ){
+			if( debug ){
+				var message_formatted = '%cTime Machine: %c' + message;
+				console[type || 'log']( message_formatted, 'font-weight:bold;', '' );
+			}
+		}
+
+		function pushStateChange( url ){
+			var stripped_href = stripTrailingSlash( url );
+			if( stripped_href === stripTrailingSlash( window.location.href ) ){
+				debugLog( 'State change matches current state, ignoring' );
+				debugLog( '------' );
+			}
+			else{
+				debugLog( 'Pushing new state "' + url + '"' );
+				window.History.pushState( {}, title_while_loading, stripped_href );
+			}
+		}
+
+		function handleStateChange(){
+			debugLog( 'State change detected' );
+			var pathname = getPathName( window.location.href );
+			if( inputs.beforeNewPageLoad ){
+				debugLog( 'Running "beforeNewPageLoad" callback' );
+				inputs.beforeNewPageLoad();
+			}
+			loadPage( pathname );
+		}
+
+		function loadPage( pathname ){
+			debugLog( 'Requesting new page "' + pathname + '"' );
+			ajax( {
+				type: 'get',
+				url: site_root + frameless_root + pathname,
+				timeout: 5000,
+				onSuccess: onLoadSuccess,
+				onError: onLoadFail
+			} );
+		}
+
+		function onLoadSuccess( data ){
+			debugLog( 'Page successfully loaded' );
+			inputs.ajax_receptacle.innerHTML = data;
+			var metadata_element = inputs.ajax_receptacle.querySelector( inputs.metadata_element_selector );
+			var title = metadata_element.getAttribute( 'data-tm-title' );
+			var page_id = metadata_element.getAttribute( 'data-tm-id' );
+			setTitle( title );
+			highlightNav( page_id );
+			bindTriggers( inputs.ajax_receptacle );
+			if( inputs.afterNewPageLoad ){
+				debugLog( 'Running "afterNewPageLoad" callback' );
+				inputs.afterNewPageLoad();
+			}
+			debugLog( 'Done' );
+			debugLog( '------' );
+		}
+
+		function onLoadFail(){
+			debugLog( 'Page failed to load, turning back time...', 'warn' );
+			window.History.back();
+			debugLog( '------' );
+		}
+
+		function setTitle( title ){
+			debugLog( 'Setting page title to "' + title + '"' );
+			title_element.innerHTML = typeof title === 'undefined' ? '' : title;
+		}
+
+		function highlightNav( page_id ){
+			debugLog( 'Highlighting navigation item "' + page_id + '"' );
+			for( var i = inputs.nav_items.length; i > 0; i -= 1 ){
+				var nav_item = inputs.nav_items[i-1];
+				var match_id = nav_item.getAttribute( 'data-tm-match' );
+				if( match_id === page_id ){
+					nav_item.className += ' ' + inputs.nav_selected_class;
+				}
+				else{
+					nav_item.className = nav_item.className.replace( regex_toggle_class, '' );
+				}
+			}
+		}
+
+		function bindTriggers( context ){
+			var triggers = context.querySelectorAll( state_change_selector );
+			debugLog( 'Binding ' + triggers.length + ' state change triggers inside "' + context.localName + ( context.id ? '#' + context.id : '' ) + '"' );
+			var pushStateChangeEvent = function( event ){
+				event.preventDefault();
+				pushStateChange( this.href );
+			};
+			for( var i = triggers.length; i > 0; i -= 1 ){
+				var trigger = triggers[i-1];
+				eventie.bind( trigger, 'click', pushStateChangeEvent );
+			}
+		}
+
+		function getPathName( url ){
+			url = url.replace( site_root, '' );
+			url = stripTrailingSlash( url );
+			return url === '' ? '/' : url;
+		}
+
+		function stripTrailingSlash( url ){
+			url = url.split( '#' )[0];
+			if( url.charAt( url.length - 1 ) === '/' ){
+				url = url.substr( 0, url.length - 1 );
+			}
+			return url;
+		}
+
+		return {
+			pushStateChange: pushStateChange
+		};
+
+	};
+
+} )();
